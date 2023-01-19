7aa80ef9f21c0579aa6660b2fca4962ede0d2071
Jeslin C John <johncjeslin@gmail.com>
2023-01-19 14:53:29 +0530

    Season Slot As unavailable

--- TurfBooking/Controllers/BookingController.cs
+++ TurfBooking/Controllers/BookingController.cs
@@ -4,6 +4,7 @@ using TurfBooking.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using Microsoft.Extensions.Configuration;
 
 namespace TurfBooking.Controllers
 {
@@ -28,7 +29,29 @@ namespace TurfBooking.Controllers
             .ToList();
 
 
+            var MonsoonStart = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("SeasonSettings")["MonsoonStart"];
+            DateTime.TryParse(MonsoonStart, out DateTime ParseMonsoonStart);
+            var MonsoonEnd = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("SeasonSettings")["MonsoonEnd"];
+            DateTime.TryParse(MonsoonEnd, out DateTime ParseMonsoonEnd);
+
+
+            
+
+
+
             GroundSlotsModel SlotObj = new GroundSlotsModel();
+
+
+            if (ParseMonsoonStart <= ParseDateTime && ParseMonsoonEnd >= ParseDateTime)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        SlotObj.GroundSlotsArr[i,j] = true;
+                    }
+                }
+            }
 
             for (int i=0; i < FillSlot.Count; i++)
             {
@@ -50,19 +73,25 @@ namespace TurfBooking.Controllers
 
                 if (SlotObj.GroundSlotsArr[0, j] == false)
                     SlotListObj.Ground1 = "Book Now!";
-                else SlotListObj.Ground1 = "Slot Filled";
+                else SlotListObj.Ground1 = "Slot Unavailable";
 
                 if (SlotObj.GroundSlotsArr[1, j] == false)
                     SlotListObj.Ground2 = "Book Now!";
-                else SlotListObj.Ground2 = "Slot Filled";
+                else SlotListObj.Ground2 = "Slot Unavailable";
 
                 if (SlotObj.GroundSlotsArr[2, j] == false)
                     SlotListObj.Ground3 = "Book Now!";
-                else SlotListObj.Ground3 = "Slot Filled";
+                else SlotListObj.Ground3 = "Slot Unavailable";
+
+
+                
+
 
                 SlotList.Add(SlotListObj);
                 }
             
+
+
 
             return View(SlotList);
         }
